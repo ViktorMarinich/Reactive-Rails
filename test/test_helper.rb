48eef994a2893d33f184d86168f43bdea8c48a69
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 01:52:28 +0200

    Configure test_helper to use selenium javascript driver. Configure selenium to work with Chrome browser.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,29 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "capybara/rails"
+require "minitest/rails/capybara"
+require 'capybara/webkit'
+require 'minitest/autorun'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.javascript_driver = :selenium
+Capybara.default_max_wait_time = 5
+include Rails.application.routes.url_helpers
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include Capybara::DSL
+end
 
-  # Add more helper methods to be used by all tests here...
+class   ActionDispatch::IntegrationTest < ActiveSupport::TestCase
+  def setup
+    Capybara.current_driver = Capybara.javascript_driver
+  end
+
+  def teardown
+    Capybara.use_default_driver
+  end
 end
