779f648a4e9096513c0c198864a1f54809dca433
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 01:57:58 +0200

    Create sign_in integration test

--- test/integration/sign_in_test.rb
+++ test/integration/sign_in_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class UsersTest < ActionDispatch::IntegrationTest
+  def setup
+    super
+  end
+
+  def teardown
+    super
+  end
+
+  def test_sign_in
+    @user =User.create(email: "1@ukr.net", name: "pampam", password: 'aaaa',
+    password_confirmation: "aaaa")
+    page.fill_in "email", :with => @user.email
+    page.fill_in "password", :with => "aaaa"
+    page.find('button[id="sign_in"]').click
+    assert page.has_content?("Welcome to User Profile")
+  end
 end
