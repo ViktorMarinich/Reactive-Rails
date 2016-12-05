f0c7d9614fc41ddfe5635e20310dd7259150e37d
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 12:38:54 +0200

    Add session helper methods to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def user_signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    return false unless user_signed_in?
+    session[:user_id] = nil
+  end
 end
