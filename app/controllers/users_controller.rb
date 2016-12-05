b366b1555f768158ac93865e85cb0f8fc30a2c00
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 16:27:28 +0200

    Add create action to users controller , disable protect from forgecy if request format is .json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,29 @@
 class UsersController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+  
   def index
   end
 
   def current
-   @user = current_user
-   render json:  @user.to_json
-end
+    @user = current_user
+    render json:  @user.to_json
+  end
+
+  def create
+    unless user_signed_in?
+      @user = User.new(user_params)
+      if @user.save
+        sign_in(@user)
+        render :json => @user.to_json
+      else
+        render :json => { :errors => @user.errors }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.required(:user).permit(:name,:email, :password, :password_confirmation, :avatar)
+  end
 end
