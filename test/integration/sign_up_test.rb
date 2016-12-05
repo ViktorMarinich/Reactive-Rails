e154deaed6e80e43929a0e42481c4c7109e03fdf
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 16:19:31 +0200

    Remove assert_difference from sign_up test

--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -11,13 +11,11 @@ class UsersTest < ActionDispatch::IntegrationTest
   end
 
   def test_create_new_user
-     assert_difference 'User.count', 1 do
-      page.fill_in "name_field", :with => "name"
-      page.fill_in "email_field", :with => "theusewr@a.net"
-      page.fill_in "password_field", :with => "password"
-      page.fill_in "password_confirmation_field", :with => "password"
-      page.find('input[id="sign_up"]').click
-    end
+    page.fill_in "name_field", :with => "name"
+    page.fill_in "email_field", :with => "theusewr@a.net"
+    page.fill_in "password_field", :with => "password"
+    page.fill_in "password_confirmation_field", :with => "password"
+    page.find('button[id="sign_up"]').click
     assert page.has_content?("Welcome to User Profile")
   end
 end
