0a08c2bee5e31b36e051c5579a5868c6651219c6
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 15:06:47 +0200

    Add visit root_path  and fixtures to setup action in sign_in_test

--- test/integration/sign_in_test.rb
+++ test/integration/sign_in_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class UsersTest < ActionDispatch::IntegrationTest
   def setup
     super
+    @user= users(:user1)
+    visit root_path
   end
 
   def teardown
@@ -10,8 +12,6 @@ class UsersTest < ActionDispatch::IntegrationTest
   end
 
   def test_sign_in
-    @user =User.create(email: "1@ukr.net", name: "pampam", password: 'aaaa',
-    password_confirmation: "aaaa")
     page.fill_in "email", :with => @user.email
     page.fill_in "password", :with => "aaaa"
     page.find('button[id="sign_in"]').click
