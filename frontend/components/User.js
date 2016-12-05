79616e78a727f9065e9565a92d33073e56fa21b1
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 22:13:03 +0200

    Add users actions , render news component

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as userActions from "../actions/userActions"
+import * as currentUserActions from "../actions/currentUserActions"
+import News from './News'
 
-export default class Friend extends React.Component{
-
+class User extends Component{
+  componentWillMount(){
+    this.props.userActions.fetchUser(this.props.params.userId)
+  }
   render() {
+    console.log('pro',this.props)
     return (
       <div>
       Welcome to User Profile
-      </div>
+      {this.props.current_user.name}
+        <News user={this.props.user} updateNews={this.props.userActions.updateNews}
+          text={this.props.text} updateNewsText={this.props.userActions.updateNewsText}/>
+        </div>
     );
   }
 }
+function mapStateToProps(state) {
+  return {
+    user: state.user.user,
+    text: state.user.text,
+    current_user: state.currentUser.currentUser,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    userActions: bindActionCreators(userActions, dispatch),
+    currentUserActions: bindActionCreators(currentUserActions, dispatch)
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(User)
