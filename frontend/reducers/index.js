f66141c6e695eb0216df19ead93426eaa2c5f306
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 18:33:08 +0200

    Add user reducer to combine reducers

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import currentUser from "./currentUserReducer"
+import user from "./userReducer"
 
 export default combineReducers({
-  currentUser
+  currentUser,
+  user
 })
