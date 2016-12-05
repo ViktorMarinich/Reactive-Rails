73673d683f4fa3b47e982a5c916e54e5a9ebd305
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 00:54:28 +0200

    Temp disable selenium-chrome configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,10 @@ require "capybara/rails"
 require "minitest/rails/capybara"
 require 'minitest/autorun'
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
+#Capybara.register_driver :selenium do |app|
+#  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
+
 Capybara.javascript_driver = :selenium
 Capybara.default_max_wait_time = 5
 include Rails.application.routes.url_helpers
