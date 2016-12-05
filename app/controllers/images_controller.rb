66a8b5a6e2650d189b420518928cffeb3f339ee5
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 01:58:45 +0200

    Add create action to images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,9 @@
 class ImagesController < ApplicationController
+
+  def create
+    @images=[]
+    params[:image].each { |k,v|  @images<< current_user.gallery.images.create(image: v) }
+    render json: @images.to_json
+  end
+  
 end
