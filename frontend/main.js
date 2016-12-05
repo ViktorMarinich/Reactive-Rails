bee0f2d5ef86f6fdc07a87e803daa0d84385ad26
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 13:42:46 +0200

    Import store component. Put all components into provider component to user redux.

--- frontend/main.js
+++ frontend/main.js
@@ -3,14 +3,18 @@ import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app'
 import User from './components/User';
+import { Provider } from 'react-redux'
+import store from './store'
 
 document.addEventListener('DOMContentLoaded', function() {
 
 ReactDOM.render(
+  <Provider store={store}>
     <Router history={hashHistory}>
         <Route path='/' component={App}>
             <IndexRoute  component={User} />
         </Route>
-    </Router>,
+    </Router>
+  </Provider>,
    document.getElementById('app'));
 });
