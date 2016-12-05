3e5fcf025aacaf6d625ffb16bded69c83cefe0e0
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 17:59:38 +0200

    Add reference has_many news to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   after_save :create_wall
   has_one :wall
+  has_many :news
   has_secure_password
 
   private
   def create_wall
     Wall.create(user_id: self.id)
   end
-  
+
   def self.bcrypt_string(string,cost=1)
     BCrypt::Password.create(string,cost: cost)
   end
