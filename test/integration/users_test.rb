b9384fc4a0c14814d2c7dfc9ef1384d20a55312d
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 01:20:56 +0200

    Testing user profile info on users page. Rename test_news to test_newst_on_user_wall

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -11,10 +11,8 @@ class UsersTest < ActionDispatch::IntegrationTest
     super
   end
 
-  def test_news
-    page.fill_in "email", :with => @user.email
-    page.fill_in "password", :with => "aaaa"
-    page.find('button[id="sign_in"]').click
+  def test_news_on_user_wall
+    sign_in
     click_on "My profile"
     assert page.has_content?("News")
     assert page.has_selector?('textarea',:count => 1)
@@ -23,5 +21,21 @@ class UsersTest < ActionDispatch::IntegrationTest
     page.find('button[id="create_news"]').click
     assert page.has_content?("New news")
     click_on "Sign Out"
+  end
+
+  def test_user_info_on_profile_page
+    sign_in
+    click_on "My profile"
+    assert page.has_selector?('img',:count => 1)
+    assert page.has_content?("Profile info")
+    assert page.has_content?(@user.name)
+    assert page.has_content?(@user.email)
+    click_on "Sign Out"
+  end
+
+  def sign_in
+    page.fill_in "email", :with => @user.email
+    page.fill_in "password", :with => "aaaa"
+    page.find('button[id="sign_in"]').click
   end
 end
