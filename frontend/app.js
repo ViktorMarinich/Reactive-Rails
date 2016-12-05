14324706ed46a00bae225b3141e60a41c96628a1
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 14:08:55 +0200

    Connect currentUser and currentUserActions to App component

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SignIn from './components/SignIn'
-export default class App extends React.Component {
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as currentUserActions from "./actions/currentUserActions"
+
+class App extends Component {
+
   render() {
+    console.log(this.props)
     if (this.props.current_user== null){
       return(<SignIn/>)}
     return (<div>{this.props.children}</div>
     );
   }
 }
+function mapStateToProps(state) {
+  return {
+    current_user: state.currentUser.currentUser,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    currentUserActions: bindActionCreators(currentUserActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
