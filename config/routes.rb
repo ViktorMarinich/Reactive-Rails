0624e9372388cf5faf0f56f6c487cee3352ec4bc
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 20:45:49 +0200

    Add  news resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'users/current' => 'users#current'
   resources :users, :defaults => { :format => 'json' }
   resources :sessions, only: [:create,:destroy], :defaults => { :format => 'json' }
+  resources :news, only: [:create ],:defaults => { :format => 'json' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
