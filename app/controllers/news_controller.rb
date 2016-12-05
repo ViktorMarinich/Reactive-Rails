e7d455e69fd2c81e89018bfec80901f30198d887
ViktorMarinich <victormarinich1@gmail.com>
2016-12-05 18:06:11 +0200

    Create news controller. Add create action . Disable protect from forgecy for json requests

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,25 @@
+class NewsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def create
+    @user=User.find(params[:id])
+    @wall=@user.wall
+    @news=@wall.news.new(text: params[:news][:text])
+    current_user.news << @news
+    respond_to do |format|
+      format.json do
+        if @news.save
+          render :json => @news.to_json
+        else
+          render :json => { :errors => @news.to_json }
+        end
+      end
+    end
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:text,:user_id,:wall_id)
+  end
+end
