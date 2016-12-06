b4372a58072d4f5b217e6f0d911a4b3a1be4ff5f
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 05:17:48 +0200

    Add Dropzone component for add images to news

--- frontend/components/News.js
+++ frontend/components/News.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import axios from "axios";
+import Dropzone from 'react-dropzone';
 
 export default class News extends React.Component{
   createNews(){
-    this.props.updateNews({id: this.props.user.id,news: {text: this.props.text}})
+    const data = new FormData();
+    this.props.news_files.map((file)=>{ data.append( `image[${file.name}]`, file)})
+    data.append('id',this.props.user.id)
+    data.append('news[text]', this.props.text)
+    this.props.updateNews(data)
     this.props.updateNewsText('')
+  }
+  onDrop(files) {
+    this.props.updateNewsFiles(files);
+  }
+  onOpenClick () {
+     this.refs.dropzone.open();
   }
   changeContent = (e) => {
     this.props.updateNewsText( e.target.value)
   }
   render() {
-    const news=(typeof this.props.user.wall=='undefined')? '': this.props.user.wall.news.sort(  function(a, b) {
+    const {news_files, user}=this.props
+    const news=(typeof user.wall=='undefined')? '': user.wall.news.sort(  function(a, b) {
     if (a.id > b.id) { return -1;}
     if (a.id < b.id) { return 1; }
     return 0; }).map( function (news) {
@@ -20,6 +32,20 @@ export default class News extends React.Component{
     <div className='border shadow'>
       <h3 className='align-center'>News</h3>
       <textarea value={this.props.text} id='news' ref='comment'  onChange={this.changeContent}  type='text' placeholder='Type yours comment' cols='32' rows='2'/>
+        <div>
+            <Dropzone ref="dropzone"   onDrop={this.onDrop.bind(this)} >
+              <div style={{height: 100}}><h4>Drop image here to upload </h4></div>
+            </Dropzone>
+            <button type="button" onClick={this.onOpenClick.bind(this)}>
+                Open file
+            </button>
+            {(news_files.length >0 )? <div >
+            <h4>You add {news_files.length} images.</h4>
+            <div >
+              {news_files.map((file) => <img  src={file.preview} width="50" height="50"/>)}
+            </div>
+            </div> : null}
+        </div>
       <button id='create_news' onClick={this.createNews.bind(this)}>Send</button>
        {news}
     </div>
