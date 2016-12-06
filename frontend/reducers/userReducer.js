d7705351ef4270bf08cc9784a54b5312f2d15f1b
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 03:13:33 +0200

    Change userReducer, userActions and SignUp extensions to .js

---
+++ frontend/reducers/userReducer.js
@@ -0,0 +1,25 @@
+export default function reducer(state={user: [], text: '',files: [] }, action) {
+    switch (action.type) {
+      case "FETCH_USER": {
+        return {...state, user: action.payload}
+        break;
+      }
+      case "UPDATE_USER": {
+        return {...state, user: {...state.user, name: action.payload.name, email: action.payload.email} }
+        break;
+      }
+      case "UPDATE_NEWS": {
+      return {...state, user: {...state.user, wall: {...state.user.wall, news: [...state.user.wall.news, action.payload]}}}
+      break;
+      }
+      case "UPDATE_NEWS_TEXT": {
+        return {...state, text: action.payload }
+        break;
+      }
+      case "UPDATE_FILES": {
+        return {...state, files: action.payload }
+        break;
+      }
+  }
+    return state
+}
