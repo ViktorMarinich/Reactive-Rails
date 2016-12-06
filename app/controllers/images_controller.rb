3f0cd6d205f6f2e60aa4d9ef75c8260088743d46
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 20:40:26 +0200

    Image json include only image and id

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,7 @@ class ImagesController < ApplicationController
   def create
     @images=[]
     params[:image].each { |k,v|  @images<< current_user.gallery.images.create(image: v) }
-    render json: @images.to_json
+    render json: @images.to_json(:only => [:image, :id])
   end
 
 end
