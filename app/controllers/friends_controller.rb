49cdc257a560872563e7c5030bbbbc47500c01bc
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 13:52:26 +0200

    Add create and destroy actions to friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,20 @@
 class FriendsController < ApplicationController
+  def create
+    @user = User.find( params[:friend_id])
+   unless @user == current_user&&!friend?(@user)
+      @friendships=current_user.friendships.new(friend: @user)
+      rel = @user.relationships.find_by(friend_id: current_user)
+      rel.destroy if rel
+    end
+    if @friendships.save
+      render json: current_user.to_json
+    end
+  end
+
+  def destroy
+    friendships = current_user.friendships.find_by(friend: params[:id])
+    if friendships.destroy
+      render json: friendships.to_json
+    end
+  end
 end
