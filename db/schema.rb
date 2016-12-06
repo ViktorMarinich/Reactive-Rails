a26172862ecca8698a6abe3c6937eac041ee6dad
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 12:55:14 +0200

    Generate relationship model as storage of incoming and outcoming friend requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206103323) do
+ActiveRecord::Schema.define(version: 20161206105057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 20161206103323) do
   add_index "news", ["user_id"], name: "index_news_on_user_id", using: :btree
   add_index "news", ["wall_id"], name: "index_news_on_wall_id", using: :btree
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "relationships", ["user_id"], name: "index_relationships_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -78,5 +87,6 @@ ActiveRecord::Schema.define(version: 20161206103323) do
   add_foreign_key "images", "galleries"
   add_foreign_key "news", "users"
   add_foreign_key "news", "walls"
+  add_foreign_key "relationships", "users"
   add_foreign_key "walls", "users"
 end
