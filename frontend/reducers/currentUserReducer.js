a506f6b90373f28deb1b4fbd9202386c1c0226c6
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 21:52:03 +0200

    Delete initial current_user state from reducer

--- frontend/reducers/currentUserReducer.js
+++ frontend/reducers/currentUserReducer.js
@@ -1,4 +1,4 @@
-export default function reducer(state={ currentUser: null }, action) {
+export default function reducer(state={ }, action) {
     switch (action.type) {
       case "FETCH_CURRENT_USER": {
         return {...state, currentUser: action.payload}
