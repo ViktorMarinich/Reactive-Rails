7e2111330f5887ea424d162f24676b8f00810c42
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 13:08:33 +0200

    Render relationship as json after save

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,8 @@ class RelationshipsController < ApplicationController
   def create
     friend = User.find(params[:friend_id])
     @relationship = current_user.relationships.new(friend: friend)
-    @relationship.save
-  end
-
-  def destroy_outcoming
-    friend = User.find(params[:friend_id])
-    current_user.outcoming.find_by(friend: friend).destroy
+    if @relationship.save
+      render json: @relationship.to_json
+    end
   end
 end
