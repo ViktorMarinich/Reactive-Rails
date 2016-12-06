fdf003bf660f8a3d1786f83cf9dcc1b29748717c
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 03:01:36 +0200

    Disable protect form forgecy for json requests

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,9 +1,10 @@
 class ImagesController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
 
   def create
     @images=[]
     params[:image].each { |k,v|  @images<< current_user.gallery.images.create(image: v) }
     render json: @images.to_json
   end
-  
+
 end
