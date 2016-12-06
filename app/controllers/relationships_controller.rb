87c429cb520bea1e9e8599fb3cb0578723084d85
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 13:07:06 +0200

    Add create relationships action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,12 @@
 class RelationshipsController < ApplicationController
+  def create
+    friend = User.find(params[:friend_id])
+    @relationship = current_user.relationships.new(friend: friend)
+    @relationship.save
+  end
+
+  def destroy_outcoming
+    friend = User.find(params[:friend_id])
+    current_user.outcoming.find_by(friend: friend).destroy
+  end
 end
