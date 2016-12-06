e61949c9f2b4ed1edce5d0289946b36d28076896
ViktorMarinich <victormarinich1@gmail.com>
2016-12-06 04:54:58 +0200

    Add  images from user gallery.Add updateGallery and updateFiles actions on axios success

--- frontend/components/Gallery.js
+++ frontend/components/Gallery.js
@@ -12,26 +12,32 @@ export default class Gallery extends React.Component{
     this.props.files.map((file)=>{ data.append( `image[${file.name}]`, file)})
      axios.post('/images', data)
      .then((response) => {
+       this.props.updateGallery(response.data)
+       this.props.updateFiles([]);
       })
-
   }
   onOpenClick () {
      this.refs.dropzone.open();
   }
   render() {
+    const {files, user}=this.props
+    const gallery= (typeof this.props.user.gallery =='undefined')?'':this.props.user.gallery.images.map(function(image){
+    return <img key={image.id} className="padding inline-block" src={image.image.smaller.url} style={{width: 72, height: 72}} ></img>
+})
     return (
       <div>
+          {gallery}
           <Dropzone ref="dropzone"   onDrop={this.onDrop.bind(this)} >
             <div style={{height: 100}}><h4>Drop image here to upload </h4></div>
           </Dropzone>
           <button type="button" onClick={this.onOpenClick.bind(this)}>
               Open file
           </button>
-          {(this.props.files.length >0 )? <div >
+          {(files.length >0 )? <div >
           <button  onClick={this.uploadImages.bind(this)}>Upload files</button>
-          <h4>You add {this.props.files.length} images.</h4>
+          <h4>You add {files.length} images.</h4>
           <div >
-            {this.props.files.map((file) => <img  src={file.preview} width="50" height="50"/>)}
+            {files.map((file) => <img  src={file.preview} width="50" height="50"/>)}
           </div>
           </div> : null}
       </div>
