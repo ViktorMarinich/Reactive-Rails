e62d9aabc3d684e3ffe94c1d4e8962c247983387
ViktorMarinich <victormarinich1@gmail.com>
2016-12-07 02:49:38 +0200

    Replace relationships with current_user in render json action. Disable protect form forgery to json requests

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,11 @@
 class RelationshipsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+  
   def create
     friend = User.find(params[:friend_id])
     @relationship = current_user.relationships.new(friend: friend)
     if @relationship.save
-      render json: @relationship.to_json
+      render json: current_user.to_json
     end
   end
 end
