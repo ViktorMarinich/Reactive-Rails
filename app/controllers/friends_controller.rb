d22b52fbfeece28498b83378ad1fdfe5f975f0ce
ViktorMarinich <victormarinich1@gmail.com>
2016-12-07 10:57:35 +0200

    Fix problem where  current user was nil

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,26 +1,26 @@
 class FriendsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
-
   def create
     @user = User.find( params[:friend_id])
-   if ((@user != current_user)&&(!current_user.friends.find_by(id: @user.id)))
-      @friendships=current_user.friendships.new(friend: @user)
-      rel = @user.relationships.find_by(friend_id: current_user)
-      rel.destroy if rel
-      if @friendships.save
-        render json: @user.to_json
-      end
+    if ((@user != current_user)&&(!current_user.friends.find_by(id: @user.id)))
+     @friendships=current_user.friendships.new(friend: @user)
+     rel = @user.relationships.find_by(friend_id: current_user)
+     rel.destroy if rel
+     if @friendships.save
+       render json: @user.to_json
+     end
     else
-      rel = @user.relationships.find_by(friend_id: current_user)
-      rel.destroy if rel
+     rel = @user.relationships.find_by(friend_id: current_user)
+     rel.destroy if rel
     end
 
   end
 
   def destroy
-    friendships = current_user.friendships.find_by(friend: params[:id])
-    if friendships.destroy
-      render json: friendships.to_json
+    @user = User.find(params[:id])
+    @friendships = @user.friendships.find_by(friend: params[:friend_id])
+    if @friendships.destroy
+      render json: @user.to_json
     end
   end
 end
