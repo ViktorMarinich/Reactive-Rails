04dec4ef52a87b1095138049961f3a4f5c403079
ViktorMarinich <victormarinich1@gmail.com>
2016-12-08 21:10:23 +0200

    Add authenticate method to before_filter for destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
+  before_filter :authenticate , only: [:destroy]
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
