c5a225473facd6a04802f66fead0fdc6483198e0
ViktorMarinich <victormarinich1@gmail.com>
2016-12-08 11:35:20 +0200

    Create 'FETCH_NEWS' action . Add break operator to 'FETCH_CURRENT_USER' action

--- frontend/reducers/currentUserReducer.js
+++ frontend/reducers/currentUserReducer.js
@@ -2,9 +2,15 @@ export default function reducer(state={ }, action) {
     switch (action.type) {
       case "FETCH_CURRENT_USER": {
         return {...state, currentUser: action.payload}
+        break;
       }
       case "DESTROY_SESSION": {
         return {...state, currentUser: action.payload}
+        break;
+      }
+      case "FETCH_NEWS": {
+        return {...state, allNews: action.payload}
+        break;
       }
     }
     return state
