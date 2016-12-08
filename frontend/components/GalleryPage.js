9229e3c63fb6682803f69b52d1c01c0921d60de6
ViktorMarinich <victormarinich1@gmail.com>
2016-12-08 04:06:22 +0200

    Create GalleryPage component that contain all users images

---
+++ frontend/components/GalleryPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import axios from "axios";
+import { Link } from 'react-router';
+
+export default class GalleryPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.infiniteLoad = this.infiniteLoad.bind(this)
+  }
+  componentWillMount(){
+      this.props.setCounter(3)
+    }
+  componentDidMount(){
+    window.addEventListener('scroll', this.infiniteLoad);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.infiniteLoad);
+  }
+  LoadMore() {
+    this.props.setCounter(this.props.counter+1)
+  }
+
+  infiniteLoad(){
+  //  if((this.props.counter - this.props.current_user.friends.length)>= 25){return null}
+   const height = document.getElementsByTagName('body')[0].clientHeight - (window.innerHeight + window.pageYOffset)
+     if (height< 40){this.LoadMore()}
+  }
+  render() {
+    const current_user= this.props.current_user
+    const images = this.props.current_user.gallery.images.slice(0,this.props.counter).map((image)=>{
+     return   <img key={image.id}  src={image.image.url} style={{width: '700px'}} ></img>
+     })
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center'}}>Friends</h1>
+        <div style={{display: 'flex', width: '770px', paddingLeft: '20px', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'space-around'}} >
+          {images}
+        </div>
+      </div>
+      )
+    }
+}
