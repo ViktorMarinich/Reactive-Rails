a87fdc451237e0d9dfc6399b4f2a7008cc3e4618
ViktorMarinich <victormarinich1@gmail.com>
2016-12-09 13:33:43 +0200

    Change updateSettings method: add if statement that check if files not exist, then dont add them to data with params. Change styles

--- frontend/components/Settings.js
+++ frontend/components/Settings.js
@@ -9,7 +9,8 @@ export default class Settings extends React.Component{
 
     updateSettings(){
       const data = new FormData();
-      data.append( `user[avatar]`, this.props.files[0])
+      if (typeof this.props.files[0] !== "undefined"){
+      data.append( `user[avatar]`, this.props.files[0])}
       data.append( `user[email]`, this.refs.email.value)
       data.append( `user[name]`, this.refs.name.value)
       axios.patch(`/users/${this.props.current_user.id}`,  data)
@@ -30,9 +31,9 @@ export default class Settings extends React.Component{
     console.log('aaa',this.props)
     const {current_user,files} = this.props
     return (
-      <div style={{display: 'flex', width: '770px', backgroundColor: 'grey',flexDirection: 'column',alignContent: 'space-around', justifyContent: 'space-around'}}>
+      <div style={{display: 'flex', width: '870px',flexDirection: 'column',alignContent: 'space-around', justifyContent: 'space-around'}}>
         <h3 style={{textAlign: 'center'}}>Settings</h3>
-        <div>
+        <div style={{ borderStyle: 'double',textAlign: 'left', backgroundColor:'#823737', paddingLeft: '100px', paddingBottom: '10px'}}>
           <h3>Name</h3>
           <p><input type='text' id='edit_name' ref='name' defaultValue={current_user.name}/></p>
           <h3>Email</h3>
@@ -42,15 +43,13 @@ export default class Settings extends React.Component{
           <Dropzone ref="dropzone" style={{display: 'none'}}  onDrop={this.onDrop.bind(this)} >
             <div style={{height: 100}}><h4>Drop image here to upload </h4></div>
           </Dropzone>
-          <p>  <button type="button" onClick={this.onOpenClick.bind(this)}>
-                Open file
-            </button></p>
-            {(files.length >0 )? <div >
-            <h4>New avatar preview.</h4>
-            <div >
-              {files.map((file) => <img  src={file.preview} width="50" height="50"/>)}
-            </div>
-            </div> : null}
+          <p><button type="button" onClick={this.onOpenClick.bind(this)}> Open file</button></p>
+          {(files.length >0 )? <div >
+          <h4>New avatar preview.</h4>
+          <div >
+            {files.map((file) => <img  src={file.preview} width="50" height="50"/>)}
+          </div>
+          </div> : null}
           <p><button id='save_changes' onClick={this.updateSettings.bind(this)}>Save</button></p>
         </div>
       </div>
