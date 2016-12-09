d0ade1529166d79c1b4cdce92ffc096c5d62c5f8
ViktorMarinich <victormarinich1@gmail.com>
2016-12-09 21:30:26 +0200

    Remove console.log , add spaces for nested nodes in div

--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -2,18 +2,16 @@ import React from 'react';
 import axios from "axios";
 
 export default class Profile extends React.Component{
-
   render() {
     const inline ={inline: 'inline-block', margin: '0',padding: '0'}
     const {name , email, id,  avatar} =this.props.user
-    console.log(this.props)
     if (typeof avatar=='undefined'){ return <div>Loading..</div>}
     return (
       <div>
-      <h3 style= {{inline, textAlign: 'center'}}>Profile info</h3>
-      <div style={{ borderStyle: 'double',textAlign: 'center', backgroundColor:'#7b936d'}}>
-        <h4 style= {{inline}}>Name: {name}</h4>
-        <img style= {{inline, marginBottom: '15px', borderStyle: 'double' , backgroundColor:'white'}} src={avatar.small.url}></img>
+        <h3 style= {{inline, textAlign: 'center'}}>Profile info</h3>
+        <div style={{ borderStyle: 'double',textAlign: 'center', backgroundColor:'#7b936d'}}>
+          <h4 style= {{inline}}>Name: {name}</h4>
+          <img style= {{inline, marginBottom: '15px', borderStyle: 'double' , backgroundColor:'white'}} src={avatar.small.url}></img>
         </div>
       </div>
     );
