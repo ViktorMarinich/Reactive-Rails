dc91e81f467953be7c5f091daaef5bc27e4cd26e
ViktorMarinich <victormarinich1@gmail.com>
2016-12-09 13:32:12 +0200

    Add styles , remove email and id infro from component

--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -4,16 +4,17 @@ import axios from "axios";
 export default class Profile extends React.Component{
 
   render() {
+    const inline ={inline: 'inline-block', margin: '0',padding: '0'}
     const {name , email, id,  avatar} =this.props.user
     console.log(this.props)
     if (typeof avatar=='undefined'){ return <div>Loading..</div>}
     return (
       <div>
-        <h3>Profile info</h3>
-        <img src={avatar.small.url}></img>
-        <h3>Name: {name}</h3>
-        <h3>Email: {email}</h3>
-        <h3>Email: {id}</h3>
+      <h3 style= {{inline, textAlign: 'center'}}>Profile info</h3>
+      <div style={{ borderStyle: 'double',textAlign: 'center', backgroundColor:'#7b936d'}}>
+        <h4 style= {{inline}}>Name: {name}</h4>
+        <img style= {{inline, marginBottom: '15px', borderStyle: 'double' , backgroundColor:'white'}} src={avatar.small.url}></img>
+        </div>
       </div>
     );
   }
