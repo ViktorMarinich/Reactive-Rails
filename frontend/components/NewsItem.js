99245796382fa610acb1964d9a91129833658894
ViktorMarinich <victormarinich1@gmail.com>
2016-12-09 21:36:43 +0200

    Refactoring component code

--- frontend/components/NewsItem.js
+++ frontend/components/NewsItem.js
@@ -12,13 +12,13 @@ export default class NewsItem extends React.Component{
   isYoutubeLink( text) {
     const url = text
     if (url != undefined || url != '') {
-        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
-        const match = url.match(regExp);
-        if (match && match[2].length >= 11&&(typeof match[2][11]=='undefined' || match[2][11] == ' ')) {
-          return match[2].slice(0,11)
-        } else {
-          return false
-        }
+      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+      const match = url.match(regExp);
+      if (match && match[2].length >= 11&&(typeof match[2][11]=='undefined' || match[2][11] == ' ')) {
+        return match[2].slice(0,11)
+      } else {
+        return false
+      }
     }
   }
   render() {
@@ -28,18 +28,19 @@ export default class NewsItem extends React.Component{
     })
     const video = (typeof this.isYoutubeLink(this.props.text) == 'boolean')? '' :  <iframe style={{marginLeft: '15px', marginBottom: '5px'}} id={this.props.id} src={`https://www.youtube.com/embed/${this.isYoutubeLink(this.props.text)}?autoplay=0`} width="262" height="230" frameBorder="0"  allowFullScreen type="text/html" ></iframe>
     return (
-    <div key={this.props.id} style={{borderStyle: 'solid', backgroundColor: 'white',borderWidth: '1px', marginTop: '5px', padding: '3px'}}>
-      <div style={{display: 'flex', flexDirection: 'row',borderBottomStyle: 'solid',borderBottomWidth: '1px', paddingBottom: '4px', justifyContent: 'flex-start',alignItems: 'flex-start'}}>
-        <img src={this.is_undefiend (this.props.url)} style={{width: '50px', height: '50px'}}></img>
-        <h5 style={{paddingLeft: '5px', marginTop: '5px'}}>User: {this.props.name}</h5>
-      </div>
-      <div style={{display: 'flex', minWidth: '300px', flexDirection: 'column', alignItems:'flex-start', justifyContent: 'flex-start'}}>
-        <h3 >{this.props.text}</h3>
-        <div>
-          {images}
+      <div key={this.props.id} style={{borderStyle: 'solid', backgroundColor: 'white',borderWidth: '1px', marginTop: '5px', padding: '3px'}}>
+        <div style={{display: 'flex', flexDirection: 'row',borderBottomStyle: 'solid',borderBottomWidth: '1px', paddingBottom: '4px', justifyContent: 'flex-start',alignItems: 'flex-start'}}>
+          <img src={this.is_undefiend (this.props.url)} style={{width: '50px', height: '50px'}}></img>
+          <h5 style={{paddingLeft: '5px', marginTop: '5px'}}>User: {this.props.name}</h5>
         </div>
-        {video}
+        <div style={{ minWidth: '300px', flexWrap: 'wrap',  alignContent: 'stretch', justifyContent: 'flex-start'}}>
+          <h3 style={{ wordBreak: 'break-all', paddingLeft: '5px', textAlign: 'justify'}}>{this.props.text}</h3>
+          <div style={{ minWidth: '300px', flexWrap: 'wrap',  alignContent: 'stretch'}}>
+            {images}
+          </div>
+          {video}
+        </div>
       </div>
-    </div>)
+    )
   }
 }
